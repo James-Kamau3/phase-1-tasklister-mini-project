c3c06c77db880d75446dcc5cbeabf541b0f3d576
James <kamaujames3705@gmail.com>
2023-03-25 18:43:50 +0300

    Completed assignments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const form = document.querySelector('#create-task-form');
+const task = document.querySelector('#tasks');
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+const formInput = document.querySelector('#new-task-description').value;
+if(formInput){
+    const newTask = document.createElement('li');
+    newTask.textContent = formInput;
+    task.appendChild(newTask);
+    form.reset();
+}
 });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
